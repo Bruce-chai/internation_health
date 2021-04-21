d6b20f93ec5ff6ed08d573f57d77aefc363971da
柴帅 <chaishuai@chaishuaideMacBook-Pro.local>
2021-04-21 09:14:03 +0800

    php原生pgconnect

--- application/admin/controller/Base.php
+++ application/admin/controller/Base.php
@@ -11,4 +11,25 @@ class Base extends Controller
     protected $baseHost = 'devel2.joyhealth.net/homehealth/index.php/Internet_health/';
     //正式地址
 //    protected $baseHost = 'devel2.joyhealth.net/homehealth/index.php/';
+
+    protected $host = '127.0.0.1';
+    protected $port = 5432;
+    protected $dbName = 'posdb';
+    protected $user = 'us_pos';
+    protected $pass = 'bszhh*6551';
+    protected $dbconn = '';
+    public function __construct()
+    {
+        parent::__construct();
+        $this->pgConnect();
+        if ($this->dbconn === false) {
+            $msg = "Failed to close connection to " . pg_host($this->dbconn) . ": " . pg_last_error($this->dbconn);
+            return ['code' => 400, 'msg' => $msg];
+        }
+    }
+
+    public function pgConnect()
+    {
+        $this->dbconn = pg_connect("host=$this->host port=$this->port dbname=$this->dbName user=$this->user password=$this->pass");
+    }
 }
